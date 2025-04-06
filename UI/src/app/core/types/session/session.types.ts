5d37151566a42fd5d4d1baf9025dba29fb7551b3
HamzaMeski <meskihamza5@gmail.com>
2025-04-06 01:34:28 +0000

    displaying sessions

--- UI/src/app/core/types/session/session.types.ts
+++ UI/src/app/core/types/session/session.types.ts
@@ -11,5 +11,5 @@ export interface SessionResponse {
 	subject: string,
 	sessionDate: Date,
 	createdAt: Date,
-	updateAt: Date
+	updatedAt: Date
 }
--- UI/src/app/features/dashboard/mentor/mentorSession/listMentorSessions/listMentorSessions.component.ts
+++ UI/src/app/features/dashboard/mentor/mentorSession/listMentorSessions/listMentorSessions.component.ts
@@ -1,38 +1,71 @@
 import {Component, OnInit} from "@angular/core";
 import {Store} from "@ngrx/store";
 import {
-	selectGetMenteesOfMentorError,
-	selectGetMenteesOfMentorLoading,
-	selectGetMenteesOfMentorResponse
-} from "../../../../../ngrx/selectors/mentoring/mentoring.selectors";
-import {getMenteesOfMentor} from "../../../../../ngrx/actions/mentoring/mentoring.actions";
+	selectGetMentorSessionsError,
+	selectGetMentorSessionsLoading,
+	selectGetMentorSessionsResponse
+} from "../../../../../ngrx/selectors/session/session.selectors";
+import {ActivatedRoute} from "@angular/router";
+import {getMentorSessions} from "../../../../../ngrx/actions/session/session.actions";
+import {CommonModule} from "@angular/common";
 
 
 @Component({
 	standalone: true,
 	selector: 'list-mentor-sessions',
 	imports: [
-
+		CommonModule
 	],
 	template: `
-        List Mentor Sessions
-	`
+        <div *ngIf="mentorSessionsLoading$ | async; else sessionsList" class="text-center py-4">
+            <p class="text-xl text-gray-500">Loading sessions...</p>
+        </div>
+
+        <ng-template #sessionsList>
+            <div *ngIf="mentorSessionsError$ | async as error; else showCards" class="text-center py-4">
+                <p class="text-xl text-red-500">Error: {{ error }}</p>
+            </div>
+
+            <ng-template #showCards>
+                <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6 p-4">
+                    <div *ngFor="let session of mentorSessionsResponse$ | async" class="bg-white shadow-lg rounded-lg overflow-hidden hover:scale-105 transform transition duration-200 ease-in-out">
+                        <div class="bg-gray-100 p-4">
+                            <h3 class="text-xl font-semibold text-gray-800">{{ session.subject }}</h3>
+                            <p class="text-sm text-gray-500">Session Date: {{ session.sessionDate | date: 'medium' }}</p>
+                        </div>
+                        <div class="p-4">
+                            <p><strong>Mentee ID:</strong> {{ session.menteeId }}</p>
+                            <p><strong>Mentor ID:</strong> {{ session.mentorId }}</p>
+                            <p><strong>Created At:</strong> {{ session.createdAt | date: 'medium' }}</p>
+                            <p><strong>Updated At:</strong> {{ session.updatedAt | date: 'medium' }}</p>
+                        </div>
+                    </div>
+                </div>
+            </ng-template>
+        </ng-template>
+	`,
 })
 export class ListMentorSessionsComponent implements OnInit {
-	menteesOfMentorResponse$
-	menteesOfMentorLoading$
-	menteesOfMentorError$
+	mentorSessionsResponse$
+	mentorSessionsLoading$
+	mentorSessionsError$
 
-	constructor(private store:Store) {
-		this.menteesOfMentorResponse$ = store.select(selectGetMenteesOfMentorResponse)
-		this.menteesOfMentorLoading$ = store.select(selectGetMenteesOfMentorLoading)
-		this.menteesOfMentorError$ = store.select(selectGetMenteesOfMentorError)
+	menteeId!: number
+
+	constructor(
+		private route: ActivatedRoute,
+		private store:Store
+	) {
+		this.mentorSessionsResponse$ = store.select(selectGetMentorSessionsResponse)
+		this.mentorSessionsLoading$ = store.select(selectGetMentorSessionsLoading)
+		this.mentorSessionsError$ = store.select(selectGetMentorSessionsError)
 	}
 
 	ngOnInit() {
-		this.store.dispatch(getMenteesOfMentor())
+		this.menteeId = Number(this.route.snapshot.paramMap.get('id'));
+		this.store.dispatch(getMentorSessions({menteeId: this.menteeId}))
 
-		this.menteesOfMentorResponse$.subscribe(val=> {
+		this.mentorSessionsResponse$.subscribe(val=> {
 			console.log(val)
 		})
 	}
